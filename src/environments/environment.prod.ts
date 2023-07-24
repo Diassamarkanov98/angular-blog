05abafb3630c46a1cbe0236ee85fb2520e480c75
Dias <diassamarkanov108@gmail.com>
2023-07-24 19:50:34 +0600

    feat: add project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDsVMzD53VTgy_zOVCPBGWj3ik2K2Y4pNw',
+  fbDbUrl: 'https://angular-blog-812bc-default-rtdb.europe-west1.firebasedatabase.app'
+};
